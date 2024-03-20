f77fa8343d03a25e3213ee2aee0371679fc4ad41
dongningsong <mathdsong@gmail.com>
2024-03-20 01:14:30 -0600

    create RF models

--- Machine-Learning/HousingPrice/main.py
+++ Machine-Learning/HousingPrice/main.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 from dotenv import load_dotenv
 load_dotenv()
 train_data_path = os.getenv("HOUSING_TRAIN")
@@ -21,3 +22,13 @@ X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test
 
 # Print the first several rows of training data:
 print(X_train.head())
+
+# define 5 random forest models:
+model_1 = RandomForestRegressor(n_estimators=50, random_state=0)
+model_2 = RandomForestRegressor(n_estimators=100, random_state=0)
+model_3 = RandomForestRegressor(n_estimators=100, criterion='absolute_error', random_state=0)
+model_4 = RandomForestRegressor(n_estimators=200, min_samples_split=20, random_state=0)
+model_5 = RandomForestRegressor(n_estimators=100, max_depth=7, random_state=0)
+
+models = [model_1, model_2, model_3, model_4, model_5]
+
