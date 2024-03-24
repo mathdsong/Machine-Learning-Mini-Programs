f492d92dd5f77f539b04f416c22b357e3523d9d1
dongningsong <mathdsong@gmail.com>
2024-03-23 20:07:23 -0600

    initial code

--- Mini-Programs/time.py
+++ Mini-Programs/time.py
@@ -1,39 +1,25 @@
 from timeit import default_timer as timer
 
-def main() :
-    choice = int(input("enter your choice (1 or 2): "))
-    maxReps = 31
-    count = 0
-    mult = 1
-    nums = [1, 2, 3, 5, 6, 7, 8, 9]
-    start = 0
-    stop = 0
-    # method 1
-    if (choice == 1) :
-        start = timer()
-        for i in range(maxReps) :
-            count = method1(count)
-        stop = timer()
-    # method 2
-    elif (choice == 2) :
-        start = timer()
-        for i in range(maxReps) :
-            mult = method2(mult)
-        stop = timer()
+# question 1
+start1 = timer()
+print ("please enter your name")
+strName = input()
+end1 = timer()
+duration1 = end1 - start1
+print (format(duration1, "0.6f"), " seconds")
+print ("welcome: ", strName)
 
-    totalTime = stop - start
-    print ("Total time: ", totalTime)
-    print("count = " , count , " mult = " , mult)
+# question 2
+start2 = timer()
+print ("please enter your occupation")
+strJob = input()
+end2 = timer()
+duration2 = end2 - start2
+print (format(duration2, "0.6f"), " seconds")
+print ("vocation: ", strJob)
 
-def method1(count) :
-    count += 2
-    return count
+print ("")
 
-def method2(mult) :
-    mult *= 2
-    return mult
-
-main()
 
 
 
@@ -82,3 +68,41 @@ print(timeit.timeit(setup=mysetup, stmt=mycode, number=10000))
 # print(time_elapsed)
 # print("Time elapsed(hh:mm:ss.ms): {}" . format(time_elapsed))
 # print("Time elapsed(hh:mm:ss.ms):" , format(time_elapsed))
+
+
+# from timeit import default_timer as timer
+
+# def main() :
+#     choice = int(input("enter your choice (1 or 2): "))
+#     maxReps = 31
+#     count = 0
+#     mult = 1
+#     nums = [1, 2, 3, 5, 6, 7, 8, 9]
+#     start = 0
+#     stop = 0
+#     # method 1
+#     if (choice == 1) :
+#         start = timer()
+#         for i in range(maxReps) :
+#             count = method1(count)
+#         stop = timer()
+#     # method 2
+#     elif (choice == 2) :
+#         start = timer()
+#         for i in range(maxReps) :
+#             mult = method2(mult)
+#         stop = timer()
+
+#     totalTime = stop - start
+#     print ("Total time: ", totalTime)
+#     print("count = " , count , " mult = " , mult)
+
+# def method1(count) :
+#     count += 2
+#     return count
+
+# def method2(mult) :
+#     mult *= 2
+#     return mult
+
+# main()
