83ee3629ddab0cce2f677fb32a7dd03a8ebef110
dongningsong <mathdsong@gmail.com>
2024-03-23 22:50:24 -0600

    generate basic statistics info for user response time

--- Mini-Programs/time.py
+++ Mini-Programs/time.py
@@ -1,108 +1,59 @@
+import pandas as pd
+import numpy as np
 from timeit import default_timer as timer
 
-# question 1
-start1 = timer()
-print ("please enter your name")
-strName = input()
-end1 = timer()
-duration1 = end1 - start1
-print (format(duration1, "0.6f"), " seconds")
-print ("welcome: ", strName)
+pd.set_option('display.max_colwidth', None)
 
-# question 2
-start2 = timer()
-print ("please enter your occupation")
-strJob = input()
-end2 = timer()
-duration2 = end2 - start2
-print (format(duration2, "0.6f"), " seconds")
-print ("vocation: ", strJob)
+mydata = {
+  'INTERVIEW QUESTIONS': ["What is Overfitting?",
+                          "How Do You Handle Missing or Corrupted Data in a Dataset?",
+                          "How Can You Choose a Classifier Based on a Training Set Data Size?",
+                          "Explain the Confusion Matrix with Respect to Machine Learning Algorithms.",
+                          "What Is a False Positive and False Negative and How Are They Significant?",
+                          "What Are the Three Stages of Building a Model in Machine Learning?",
+                          "What Are the Differences Between Machine Learning and Deep Learning?",
+                          "What is the Difference Between Supervised and Unsupervised Machine Learning?",
+                          "Compare K-means and KNN Algorithms.",
+                          "What is a Random Forest?"],
+  'RESPONSE DURATION': [0.0] * 10
+}
 
-print ("")
+df = pd.DataFrame(mydata)
+df.index = np.arange(1, len(df) + 1)
+print('******************************** ORIGINAL DATAFRAME ********************************')
+print(df)
+
+avg_response_duration = 0
+
+for i in range(len(mydata['INTERVIEW QUESTIONS'])):
+  start = timer()
+  print(mydata['INTERVIEW QUESTIONS'][i])
+  yourInput = input()
+  end = timer()
+  duration = end - start
+  avg_response_duration += duration
+  print ("response time for question #{} is: {} seconds".format(i+1, duration))
+  df.loc[i + 1, ['RESPONSE DURATION']] = duration
 
 
+print('******************************** UPDATED DATAFRAME ********************************')
+print(df)
 
+print('******************************** STATISTICS ********************************')
+# Generate Statistics:
+avg_response_duration /= len(mydata['INTERVIEW QUESTIONS'])
+duration_series = df['RESPONSE DURATION']
+std = duration_series.std()
+print('average response duration: ', avg_response_duration)
+print('standard deviation of the responsing duration is: ', std)
 
 
 """
-related links:
 1. https://www.guru99.com/timeit-python-examples.html
-2. example from https://www.geeksforgeeks.org/timeit-python-examples/:
-
-# importing the required module
-import timeit
-
-# code snippet to be executed only once
-mysetup = "from math import sqrt"
-
-# code snippet whose execution time is to be measured
-mycode = '''
-def example():
-    mylist = []
-    for x in range(100):
-        mylist.append(sqrt(x))
-'''
-
-# timeit statement
-print(timeit.timeit(setup=mysetup, stmt=mycode, number=10000))
-
+2. https://www.geeksforgeeks.org/timeit-python-examples/
 3. https://stackoverflow.com/questions/63757763/timeit-and-its-default-timer-completely-disagree
+4. https://www.simplilearn.com/tutorials/machine-learning-tutorial/machine-learning-interview-questions
+5. https://stackoverflow.com/questions/32249960/start-row-index-from-1-instead-of-zero-without-creating-additional-column-in-pan
+6. https://stackoverflow.com/questions/68490745/how-to-display-the-full-text-of-a-column-in-pandas
+7. https://stackoverflow.com/questions/37725195/pandas-replace-values-based-on-index
 """
-
-# from datetime import datetime
-# import timeit
-# s = '''
-# def f():
-#   myList = []
-#   for n in range(5):
-#       myList.append(n)
-# '''
-# t = timeit.timeit(stmt=s)
-# print (t, "seconds")
-
-# start_time = datetime.now()
-# print("input some words:")
-# x = input()
-
-# time_elapsed = datetime.now() - start_time
-# print(time_elapsed)
-# print("Time elapsed(hh:mm:ss.ms): {}" . format(time_elapsed))
-# print("Time elapsed(hh:mm:ss.ms):" , format(time_elapsed))
-
-
-# from timeit import default_timer as timer
-
-# def main() :
-#     choice = int(input("enter your choice (1 or 2): "))
-#     maxReps = 31
-#     count = 0
-#     mult = 1
-#     nums = [1, 2, 3, 5, 6, 7, 8, 9]
-#     start = 0
-#     stop = 0
-#     # method 1
-#     if (choice == 1) :
-#         start = timer()
-#         for i in range(maxReps) :
-#             count = method1(count)
-#         stop = timer()
-#     # method 2
-#     elif (choice == 2) :
-#         start = timer()
-#         for i in range(maxReps) :
-#             mult = method2(mult)
-#         stop = timer()
-
-#     totalTime = stop - start
-#     print ("Total time: ", totalTime)
-#     print("count = " , count , " mult = " , mult)
-
-# def method1(count) :
-#     count += 2
-#     return count
-
-# def method2(mult) :
-#     mult *= 2
-#     return mult
-
-# main()
